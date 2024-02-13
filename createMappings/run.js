93da9dba7ffa9672ac7f037c515383b67255f32c
Kenny Allen <kenny.allen996@gmail.com>
2024-02-13 17:21:14 +0000

    testing

--- createMappings/run.js
+++ createMappings/run.js
@@ -6,8 +6,8 @@ const sleep = (ms) => {
 process.on('message', async (data) => {
     console.log(data)
     if(data.start){
-        for(let i = 0; i<=5; i++){
-            await sleep(1000)
+        for(let i = 0; i<=100; i++){
+            if(i < 5){await sleep(1000)}
             process.send({message:i})
         }
         process.exit();
--- secondTestScript/run.js
+++ secondTestScript/run.js
@@ -2,10 +2,13 @@ const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 process.on('message', async (data) => {
-    for(let i = 0; i<=10; i++){
-        await sleep(1000)
-        process.send({message:i})
-        if (i==5){throw new Error('')}
+    if(data.start){
+        for(let i = 0; i<=10; i++){
+            await sleep(1000)
+            process.send({warning:i})
+            if (i==5){throw new Error('')}
+        }
+        process.exit();
     }
-    process.exit();
+
 })
